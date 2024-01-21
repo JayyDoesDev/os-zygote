d004e424d63848d30c6bdbeee1b68f7ae79eb8a3
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-01-21 12:13:40 -0500

    Delete plugins directory

--- plugins/Language/LanguagePlugin.ts
+++
@@ -1,8 +0,0 @@
-import { DefinePlugin, Plugin } from "../../DefinePlugin";
-
-export = DefinePlugin({
-  name: "Language",
-  description: "Change the language of Zygote!",
-  commands: [],
-  public_plugin: true
-}) as Plugin;
--- plugins/UrlShortening/UrlShortenPlugin.ts
+++
@@ -1,9 +0,0 @@
-import { DefinePlugin, Plugin } from "../../DefinePlugin";
-import { ShortenCommand } from "../../UrlShortening";
-
-export = DefinePlugin({
-  name: "Url Shortening",
-  description: "Shorten urls!",
-  commands: [ShortenCommand],
-  public_plugin: true
-}) as Plugin;
