8721a5a6e2c3fc2f7a9326f8355715ee2defd1f2
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-01-21 11:45:30 -0500

    chore: Finished shorten command.

--- UrlShortening/Commands/ShortenCommand.ts
+++ UrlShortening/Commands/ShortenCommand.ts
@@ -5,29 +5,49 @@ import { ChatInputCommandInteraction } from "discord.js";
 import { Status } from "../../api/Status";
 
 export const ShortenCommand = DefineCommand({
-	command: {
-		name: "shorten",
-		type: ApplicationCommandType.CHAT_INPUT,
-		description: "Shorten a url of your choice!",
-		options: [
-			{
-				type: ApplicationCommandOptionType.STRING,
-				name: 'url',
-				description: 'Enter the url you want to shorten!',
-				required: true
-			}
-		]
-	},
-	on: async (ctx: Context, interaction: ChatInputCommandInteraction) => {
-		if (!interaction.isCommand()) {
-			return;
-		}
-		
-		const url: string = interaction.options.getString("url");
-		const shorten: Status = await ctx.api.shorten(interaction.user.id, url);
+  command: {
+    name: "shorten",
+    type: ApplicationCommandType.CHAT_INPUT,
+    description: "Shorten a url of your choice!",
+    options: [
+      {
+        type: ApplicationCommandOptionType.STRING,
+        name: 'url',
+        description: 'Enter the url you want to shorten!',
+        max_length: 2000,
+        required: true
+      }
+    ]
+  },
+  on: async (ctx: Context, interaction: ChatInputCommandInteraction) => {
+    if (!interaction.isCommand()) {
+      return;
+    }
 
-		interaction.reply({
-			content: shorten.message
-		})
-	}
+    const url: string = interaction.options.getString("url");
+    const shorten: Status = await ctx.api.shorten(interaction.user.id, url);
+    switch (shorten.status) {
+      case 428:
+        return interaction.reply({
+          content: "Your url didn't seem valid <:stare:860590417245241395>",
+          ephemeral: true
+        });
+      case 400:
+        return interaction.reply({
+          content: "Looks like a problem happened on our side, please try again later! <:shock:867330764713492491>",
+          ephemeral: true
+        });
+      default:
+        return interaction.reply({
+          content: `
+          # <:SenkoWave:861323878056067132> Your url as been shortened!
+          # Old url
+          ## <${url}>
+          # New Shortened url
+          ## <${shorten.data.url}>
+          `,
+          ephemeral: true
+        });
+    };
+  },
 }) as Command;
