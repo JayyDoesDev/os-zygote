eb92804248cfae333ea9a99239886a1dc1e4a55f
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2024-02-13 23:15:39 -0500

    Delete Plugins/Actions directory

--- Plugins/Actions/ActionsPlugin.ts
+++
@@ -1,9 +0,0 @@
-import { DefinePlugin, Plugin } from "../../DefinePlugin";
-import { ActionCommand } from "../../Actions";
-
-export = DefinePlugin({
-	name: "Actions",
-	description: "Action commands!",
-	commands: [ActionCommand],
-	public_plugin: true
-}) as Plugin;
